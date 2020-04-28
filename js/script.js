a57aa5b507bb5f8ec1f04e2f05e70e9274c1eb5a
Lucas Assunção <assuncaolucasm@gmail.com>
2020-04-28 18:57:48 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initScrollSuave from './modules/scroll-suave.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFethAnimais from './modules/fetch-animais.js';
+import initFethBitcoin from './modules/fetch-bitcoin.js';
+
+initAnimacaoScroll();
+initScrollSuave();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropDownMenu();
+initMenuMobile();
+initFuncionamento();
+initFethAnimais();
+initFethBitcoin();
