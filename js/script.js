ce92c0f460b7c42268e43c32771cf3a7ba4141be
Lucas Assunção <assuncaolucasm@gmail.com>
2020-05-26 15:49:57 -0300

    Ajustando caminho da API de Numeros

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ const funcionamento = new Funcionamento('[data-semana', 'aberto');
 funcionamento.init();
 
 
-fetchAnimais('./animaisapi.json', '.numeros-grid');
+fetchAnimais('https://moraislucas.github.io/animais-fantatiscos/animais-api.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
